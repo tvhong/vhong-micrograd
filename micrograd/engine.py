96c6408de0bdd55d93fdda085e01eb46509cd76f
Vy Hong <vy.hong@dsit.gov.uk>
2026-03-09 21:09:00 +0000

    Implement add method

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,7 +1,7 @@
 class Value:
-    """ stores a single scalar value and its gradient """
+    """stores a single scalar value and its gradient"""
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
         self.grad = 0
         self._backward = lambda: None
@@ -10,3 +10,17 @@ class Value:
 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other) -> "Value":
+        if not isinstance(other, Value):
+            other = Value(other)
+
+        parent = Value(self.data + other.data, _children=(self, other), _op="+")
+
+        def _backward():
+            other.grad += parent.grad
+            self.grad += parent.grad
+
+        parent._backward = _backward
+
+        return parent
--- test/test_engine.py
+++ test/test_engine.py
@@ -13,3 +13,22 @@ def test_init():
 def test_repr():
     a = Value(3.0)
     assert repr(a) == "Value(data=3.0, grad=0)"
+
+
+def test_add():
+    # micrograd
+    a = Value(2.0)
+    b = Value(3.0)
+    c = a + b
+    c.grad = 1.0
+    c._backward()
+
+    # pytorch
+    at = torch.tensor(2.0, requires_grad=True)
+    bt = torch.tensor(3.0, requires_grad=True)
+    ct = at + bt
+    ct.backward()
+
+    assert c.data == ct.data.item()
+    assert a.grad == at.grad.item()
+    assert b.grad == bt.grad.item()
