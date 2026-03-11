6bdb8f72cd92c0244c049b8bb6459fc63f079ba9
Vy Hong <vy.hong@dsit.gov.uk>
2026-03-11 19:47:21 +0000

    Implement Module class

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,1 +1,35 @@
+import random
+
 from micrograd.engine import Value
+
+
+class Module:
+    def __init__(self, n_inputs: int) -> None:
+        self._weights: list[Value] = []
+        for i in range(n_inputs):
+            self._weights.append(self._sample())
+
+        self._bias: Value = self._sample()
+
+    def __call__(self, input: list[float]) -> Value:
+        assert len(input) == len(self._weights), (
+            "Number input elements must match configured inputs"
+        )
+
+        out = 0
+        for x, w in zip(input, self._weights):
+            out += w * x
+
+        out += self._bias
+
+        return out
+
+    def parameters(self) -> list[Value]:
+        return self._weights + [self._bias]
+
+    def zero_grad(self):
+        for param in self.parameters():
+            param.grad = 0
+
+    def _sample(self, low: float = -1, high: float = 1) -> Value:
+        return Value(random.uniform(low, high))
